d3075eeff53f53afbe35ff73894385f0b0525031
ChaiLam <110351071+Chailam0310@users.noreply.github.com>
2024-11-19 22:55:30 +0800

    objective updated

---
+++ Assets/Scripts/Chai/LetterInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterInteraction : MonoBehaviour
+{
+    public float interactionDistance;
+    public GameObject interactionText;
+    public LayerMask interactionLayers;
+
+    public bool HasInteracted { get; private set; } // 用于检测玩家是否已与信件交互
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, interactionLayers))
+        {
+            if (hit.collider.gameObject.GetComponent<letter>())
+            {
+                interactionText.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    hit.collider.gameObject.GetComponent<letter>().openCloseLetter();
+                    HasInteracted = true; // 设置为 true 表示已与信件交互
+                }
+            }
+            else
+            {
+                interactionText.SetActive(false);
+            }
+        }
+        else
+        {
+            interactionText.SetActive(false);
+        }
+    }
+}
