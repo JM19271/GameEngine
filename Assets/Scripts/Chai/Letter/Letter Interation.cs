05c4bab2401509b410f459bfd148e6ccf7c9217f
JM <lionofthenight9926@gmail.com>
2024-11-22 18:47:37 +0400

    22/11

--- Assets/Scripts/Chai/Letter/Letter Interation.cs
+++ Assets/Scripts/Chai/Letter/Letter Interation.cs
@@ -1,47 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Letterinteraction : MonoBehaviour
+public class LetterInteraction : MonoBehaviour
 {
-    //The distance from which the player can interact with an object
     public float interactionDistance;
-
-    //Text or crosshair that shows up to let the player know they can interact with an object they're looking at
     public GameObject interactionText;
-
-    //Layers the raycast can hit/interact with. Any layers unchecked will be ignored by the raycast.
     public LayerMask interactionLayers;
 
-    //The Update() void is used to make stuff happen every frame
+    public bool HasInteracted { get; private set; } // 用于检测玩家是否已与信件交互
+
     void Update()
     {
-        //RaycastHit variable which will collect information from objects the raycast hits
         RaycastHit hit;
 
-        //If the raycast hits something,
         if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, interactionLayers))
         {
-            //If the object it hit contrains the letter script,
             if (hit.collider.gameObject.GetComponent<letter>())
             {
-                //The interaction text will enable
                 interactionText.SetActive(true);
 
-                //If the E key is pressed,
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    //The letter component is accessed and the letter will open or close
                     hit.collider.gameObject.GetComponent<letter>().openCloseLetter();
+                    HasInteracted = true; // 设置为 true 表示已与信件交互
                 }
             }
-            //else, the interaction text is set false.
             else
             {
                 interactionText.SetActive(false);
             }
         }
-        //else, the interaction text is set false.
         else
         {
             interactionText.SetActive(false);
